578a5068d5ed8e91928a8a38c92d29ad17341436
Charlie Depner <63475767+CG2979@users.noreply.github.com>
2026-01-22 15:42:30 -0500

    Add merge files tab to the Streamlit app

--- survey_cleaner/app.py
+++ survey_cleaner/app.py
@@ -1,6 +1,6 @@
 """
 Main Streamlit application entry point.
-This file should be thin - it just orchestrates components.
+This file just orchestrates components.
 """
 
 import streamlit as st
@@ -9,6 +9,7 @@ from components.sidebar import render_sidebar
 from components.title_cleaning_tab import render_title_cleaning_tab
 from components.data_overview_tab import render_data_overview_tab
 from components.export_tab import render_export_tab
+from components.merge_files_tab import render_merge_files_tab
 
 # App setup
 st.set_page_config(page_title="Survey Data Cleaning App", layout="wide")
@@ -19,17 +20,22 @@ initialize_session_state()
 # Render sidebar
 render_sidebar()
 
-# Check if data is loaded
+# Check if data is loaded (not required for merge tab)
 if st.session_state["df"] is None:
     st.title("Survey Data Cleaner")
-    st.info("Upload a CSV file to get started")
+    st.info("Upload a CSV file to get started, or go to the 'Merge Files' tab to merge two files")
+    
+    # Show only merge tab if no data loaded
+    tab_merge = st.tabs(["Merge Files"])[0]
+    with tab_merge:
+        render_merge_files_tab()
     st.stop()
 
 # Main content
 st.title("Survey Data Cleaner")
 
 # Render tabs
-tab1, tab2, tab3 = st.tabs(["Title Cleaning", "Data Overview", "Export"])
+tab1, tab2, tab3, tab4 = st.tabs(["Title Cleaning", "Data Overview", "Export", "Merge Files"])
 
 with tab1:
     render_title_cleaning_tab()
@@ -39,3 +45,6 @@ with tab2:
 
 with tab3:
     render_export_tab()
+
+with tab4:
+    render_merge_files_tab()
